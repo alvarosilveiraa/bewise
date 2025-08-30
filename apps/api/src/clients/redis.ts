a0a0db4435f1cf992445f5ae2268ca77bb885a42
Alvaro Silveira <alvarosilveiraa@gmail.com>
2025-08-30 15:49:05 -0300

    feat: added redis family 0

--- apps/api/src/clients/redis.ts
+++ apps/api/src/clients/redis.ts
@@ -1,6 +1,5 @@
 import Redis from "ioredis";
-import { IS_DEV } from "@/constants/IS_DEV";
 
 export const redis = new Redis(process.env.REDIS_URL!, {
-  tls: IS_DEV ? undefined : {},
+  family: 0,
 });
