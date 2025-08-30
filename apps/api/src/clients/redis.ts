466b0ffcd12de375f32208db7def587bd614769a
Alvaro Silveira <alvarosilveiraa@gmail.com>
2025-08-30 15:44:17 -0300

    fix: adjust redis tls

--- apps/api/src/clients/redis.ts
+++ apps/api/src/clients/redis.ts
@@ -2,5 +2,5 @@ import Redis from "ioredis";
 import { IS_DEV } from "@/constants/IS_DEV";
 
 export const redis = new Redis(process.env.REDIS_URL!, {
-  tls: IS_DEV ? undefined : { rejectUnauthorized: false },
+  tls: IS_DEV ? undefined : {},
 });
