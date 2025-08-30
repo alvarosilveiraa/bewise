087afd4aff8e0e918e21b2065eed3593791e273e
Alvaro Silveira <alvarosilveiraa@gmail.com>
2025-08-30 15:40:25 -0300

    feat: added tls to redis

--- apps/api/src/clients/redis.ts
+++ apps/api/src/clients/redis.ts
@@ -1,3 +1,6 @@
 import Redis from "ioredis";
+import { IS_DEV } from "@/constants/IS_DEV";
 
-export const redis = new Redis(process.env.REDIS_URL!);
+export const redis = new Redis(process.env.REDIS_URL!, {
+  tls: IS_DEV ? undefined : { rejectUnauthorized: false },
+});
